9de49a4b8a1d28c2ed93898d64b4f646106b058a
Dima <80126225+ForestTiger-GH@users.noreply.github.com>
2026-09-11 02:34:13 +0300

    Add secure DOCX XML helper

---
+++ src/MiniDoc/Docx/DocxXml.cs
@@ -0,0 +1,36 @@
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace MiniDoc.Docx;
+
+internal static class DocxXml
+{
+    internal static XDocument Load(Stream stream, long maxCharacters)
+    {
+        var settings = new XmlReaderSettings
+        {
+            DtdProcessing = DtdProcessing.Prohibit,
+            XmlResolver = null,
+            MaxCharactersInDocument = maxCharacters,
+            IgnoreWhitespace = false,
+            CloseInput = false
+        };
+        using var reader = XmlReader.Create(stream, settings);
+        return XDocument.Load(reader, LoadOptions.PreserveWhitespace);
+    }
+
+    internal static byte[] Save(XDocument document)
+    {
+        using var memory = new MemoryStream();
+        var settings = new XmlWriterSettings
+        {
+            Encoding = new UTF8Encoding(false),
+            Indent = false,
+            OmitXmlDeclaration = false,
+            CloseOutput = false
+        };
+        using (var writer = XmlWriter.Create(memory, settings)) document.Save(writer);
+        return memory.ToArray();
+    }
+}
