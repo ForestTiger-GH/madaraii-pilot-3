6447397fe48e2afc1875dc129bac5045ddf3a7b7
Dima <80126225+ForestTiger-GH@users.noreply.github.com>
2026-09-15 16:37:23 +0300

    Return governed startup open success

--- src/MiniDoc/MainWindow.Startup.cs
+++ src/MiniDoc/MainWindow.Startup.cs
@@ -2,5 +2,10 @@ namespace MiniDoc;
 
 public partial class MainWindow
 {
-    public Task OpenInitialPathAsync(string path) => OpenPathAsync(path);
+    public Task<bool> OpenInitialPathAsync(string path)
+    {
+        var intent = _openTransitions.BeginOpenIntent();
+        var ticket = _openTransitions.Capture(intent);
+        return OpenPathAsync(path, ticket);
+    }
 }
